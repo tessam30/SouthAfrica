17a10a6cb54bf6ee62f1c0763eea02fc24a725e6
Laura Hughes <flaneuse@users.noreply.github.com>
2017-07-18 15:03:05 -0400

    LH edits w/ new data from MM, 7-18-17

---
+++ 02_reshape_HIVdata_v2.R
@@ -0,0 +1,249 @@
+# Reshape HIV Data from South Africa Mission -----------------------------------------
+#
+# 02_reshape_HIVdata_v2.R
+#
+# Script to ingest shapefiles and reshape into a long table
+# 
+# Data are from the Mashudo Mosamba, USAID South Africa
+#
+# Tim Essam, tessam@usaid.gov; Modified by Laura Hughes, lhughes@usaid.gov
+#
+# Copyright 2017 by Tim Essam via MIT License
+#
+# -------------------------------------------------------------------------
+
+
+# Overview ----------------------------------------------------------------
+# 1. Import prevalence data and swing to long format to isolate quarter/year data
+# 2. Swing back to wide format to have variable value by each separate indicator, 
+# e.g. HIV negative tests, HIV positive tests, positivity rate, etc.
+
+
+# setup -------------------------------------------------------------------
+
+# Load required libraries
+library(tidyverse)
+library(foreign)
+library(stringr)
+library(readxl)
+library(data.table)
+library(RColorBrewer)
+library(forcats)
+library(knitr)
+library(lubridate)
+
+# where data is located
+# where csvs are located
+base_dir = "~/Documents/GitHub/SouthAfrica/"
+# where prevalence data is located
+geo_dir = "~/Documents/South Africa/shp/"
+
+# start month for each quarter's data reporting
+# Q12017 is actually 10-10-2016 to 12-31-2016, for instance. 
+# Based off of fiscal year, not calendar; need data frame to map the values to
+quarter2months = data.frame(quarter = c('Q1', 'Q2', 'Q3', 'Q4'), month = c(10, 1, 4, 7))
+
+# name of the variable that encodes 'prevalence'.  In past, has been "Prev" or "PosRat"
+prev_var = "PosRat"
+pos_var = "HTSPos" # number tested positive
+neg_var = "HTSNeg" # number tested negative
+tested_var = "HTSTst" # total number tested
+
+# import data -------------------------------------------------------------
+
+
+# 2016 and 2017 data merged together, unlike in previous cuts of data
+df <- read.dbf(paste0(geo_dir, "HTS_2016_2017.dbf"))
+df_latlon <- read.csv(paste0(base_dir, "Facility_corrected_latlon.csv")) %>% 
+  na.omit()
+
+# Data frame to filter out excess clinics that are not priority ones
+df_muni <- read_excel(paste0(base_dir, "Priority_districts_filter.xlsx")) %>% 
+  rename(SNU2 = `Organisation unit`)
+
+
+# Insert a stub before the first Q (indicating to fiscal quarter) allow splitting in reshape
+colnames(df) <- gsub('Q', '_Q', colnames(df))
+
+# Reshaping to get a long data frame for ArcMap Geodatabase table
+df_PEPFAR = df %>% 
+  gather(key = indicator, value = value, -OU, -contains('SNU'), -contains('Facility'), -Latitude, -Longitude) %>% 
+  # split indicator and quarter/year data
+  separate(indicator, into = c('indicator', 'quarter_year'), sep = '_') %>% 
+  # split time into quarter/year (after 2nd digit)
+  # Create two new variables to capture time dimension (FY Quarter and Year)
+  separate(quarter_year, into = c('quarter', 'year'), sep = 2) %>% 
+  
+  mutate(
+    # convert quarter to month
+    month = plyr::mapvalues(quarter, from = quarter2months$quarter, to = quarter2months$month),
+    
+    # make sure month, year are numbers
+    month = as.numeric(month),
+    year = as.numeric(year),
+    
+    # create time var = PosiX object of year - month - 01
+    # NOTE: if quarter == Q1, shift the year to accomodate fiscal year
+    timeVar = ifelse(quarter == 'Q1', (paste(year - 1, month, '01', sep = '-')),
+                     (paste(year, month, '01', sep = '-'))),
+    timeVar = ymd(timeVar)) %>% 
+  
+  # spread indicator wide now that quarter/year has been isolated
+  spread(indicator, value) %>% 
+  
+  # flag wonky vars
+  mutate_(prevFlag = paste0('ifelse(', prev_var, ' > 100 | ', prev_var, '< 0, 1, 0)'),
+          testedFlag = paste0('ifelse(', pos_var, '+', neg_var, '!=', tested_var, ', 1, 0)')) %>% 
+  mutate(
+    priorityFlag = ifelse(SNU2 %in% df_muni$SNU2, 1, 0),
+    mobileFac = ifelse(Facility %like% "Mobile", 1, 0), 
+    locationFlag = ifelse(Longitude == 0, 1, 0)) %>% 
+  
+  # calculate one quarter lag
+  group_by(Facility) %>% 
+  arrange(timeVar) %>% 
+  mutate_(prevQ_pos = paste0('lag(', prev_var, ')'),
+          prevQ_diff = paste0(prev_var, '- prevQ_pos'),
+          prevQ_chg = 'prevQ_diff / prevQ_pos')
+    
+  
+
+
+# Fix facility locations that have improper latitude and longitude coordinates
+df_latlon_sub <- select(df_latlon, Facility, Latitude, Longitude) %>%
+  rename(Facility_y = Facility,
+         Latitude_y = Latitude,
+         Longitude_y = Longitude)
+
+df_PEPFAR = df_PEPFAR %>%
+  left_join(df_latlon_sub, c("Facility" = "Facility_y")) %>%
+  mutate(Latitude = ifelse(is.na(Latitude_y), Latitude, Latitude_y),
+         Longitude = ifelse(is.na(Longitude_y), Longitude, Longitude_y)) 
+
+# Verify that the priority facility flag is accurate
+df_PEPFAR %>% 
+  filter(priorityFlag == 1) %>%
+  group_by(SNU1, SNU2) %>% 
+  summarise(n = n())
+
+# Facility roster --> assuming that the Facility name is distinct
+df_PEPFAR %>% group_by(timeVar) %>% 
+  count(Facility) %>% 
+  filter(n >1)
+
+facilities = df_PEPFAR %>% 
+  filter(prevFlag == 0, priorityFlag == 1) %>% 
+  distinct(Facility, Latitude, Longitude)
+
+# Check that values make sense
+summary(df_PEPFAR)
+
+# Tally those records that are over 100
+df_PEPFAR %>% 
+  filter(prevFlag == 1) %>% 
+  select_('Facility', 'SNU1', 'SNU2', 'timeVar', prev_var) 
+
+
+# Look at SNU2 averages by quarter over time
+df_snu2 = df_PEPFAR %>% 
+  filter(priorityFlag == 1, locationFlag == 0, prevFlag == 0, mobileFac == 0) %>% 
+  group_by(SNU1, timeVar) %>% 
+  mutate_(prev_SNU1_qtr = paste0('sum(', pos_var, ') / sum(', tested_var, ')'), 
+         facCountSNU1 = 'n()') %>% 
+  ungroup() %>% 
+  mutate(sortVarSNU1 = fct_reorder(SNU1, -prev_SNU1_qtr)) %>% 
+  group_by(SNU2) %>% 
+  mutate_(tot_prev_SNU2 = paste0('sum(', pos_var, ') / sum(', tested_var, ')')) %>%
+  ungroup() %>% 
+  mutate(sortVarSNU2 = fct_reorder(SNU2, -tot_prev_SNU2)) %>% 
+  group_by(SNU2, timeVar) %>% 
+  mutate_(prev_SNU2_qtr = paste0('sum(', pos_var, ') / sum(', tested_var, ')'),
+         facCountSNU2 = 'n()')
+
+calc_pos = function(pos_var, tested_var) {
+  sum(pos_var) / sum(tested_var)
+}
+
+# Check how the number of facilities varies aross time
+df_snu2 %>% 
+  group_by(SNU1) %>% 
+  mutate(facCount = n()) %>% 
+  ungroup() %>% 
+  group_by(SNU1, timeVar, facCount) %>% 
+  summarize_(n = 'n()', totsize = paste0('sum(', tested_var,')')) %>% 
+  ungroup() %>% 
+  mutate(sortOrd = fct_reorder(factor(SNU1), -facCount)) %>% 
+  ggplot(., aes(x = timeVar, y = n, colour = sortOrd)) +
+  geom_line() +
+  geom_point(aes(size = totsize)) +
+  facet_wrap(~sortOrd, nrow = 2) +
+  theme_minimal() +
+  theme(legend.position = "none")
+
+
+# Show differences from SNU1 average prevalence by SNU2, sort the values as well  
+ggplot(df_snu2, aes(x = timeVar, y = prev_SNU2_qtr, colour = factor(SNU1))) +
+  geom_line() + scale_color_brewer(palette = "Set2")+
+  geom_line(aes(x = timeVar, y = prev_SNU1_qtr), colour = "gray", size = 1.25, alpha = 0.5) +
+  geom_point(aes(fill = SNU1)) +
+  facet_wrap(sortVarSNU1 ~ sortVarSNU2, nrow = 3) +
+  theme_minimal() +
+  theme(legend.position = "none", 
+        strip.text.x = element_text(size = 7),
+        axis.text.x = element_text(size = 6)) +
+  labs(title = "Gauteng Province has some of the highest HIV Prevalence rates across PEPFAR locations", 
+       subtitle = "Gray line indicates district average by quarter", 
+       y = "", x = "") +
+  scale_y_continuous(labels = scales::percent)
+
+
+# Is there still a drastic drop in prevalence rates?
+# Plotting mean positivity at SNU1 and SNU2
+df_PEPFAR %>%
+  filter(priorityFlag == 1, prevFlag == 0, mobileFac == 0) %>%
+  group_by(SNU1) %>%
+  mutate_(meanSNU1 = paste0('sum(', pos_var, ') / sum(', tested_var, ')')) %>%
+  ungroup() %>%
+  mutate(SNU1ord = fct_reorder(SNU1, -meanSNU1)) %>%
+  group_by(timeVar, SNU2, SNU1, SNU3, SNU1ord) %>%
+  summarise_(mean = paste0('mean(', prev_var, ')'),
+            n = 'n()')%>%
+  ggplot(., aes(x = timeVar, y = mean, colour = SNU3)) +
+  geom_line(alpha = 0.25)+
+  geom_smooth(colour = "gray70", alpha = 0.25, size = 1) +
+  facet_wrap(~SNU1ord, ncol = 4) +
+  theme_minimal() +
+  theme(legend.position = "none") +
+  labs(title = "HIV Test counts averaged by SNU3",
+       ylab = "HIV Test Counts", x = "")
+
+
+# Create a slippy map of the facilities
+# Question: What to do about the mobile facilities?
+library(htmlwidgets)
+library(htmltools)
+library(leaflet)
+facilities %>% 
+  filter(Longitude!= 0) %>%  
+  leaflet() %>% 
+  addProviderTiles(providers$CartoDB.Positron) %>%
+  addMarkers(~Longitude, ~Latitude, 
+             clusterOptions = markerClusterOptions(), 
+             popup = ~htmlEscape(Facility)) %>%
+  addMiniMap(
+    tiles = providers$CartoDB.Positron,
+    toggleDisplay = TRUE) 
+
+
+
+# Save for importing into Geodatabase table
+write.csv(df_PEPFAR, "HIV_Prevalence_long_2017-07-18.csv")
+
+df_subset = df_PEPFAR %>%
+  filter(prevFlag == 0, priorityFlag == 1, locationFlag == 0, mobileFac == 0, testedFlag == 0)
+write.csv(df_subset, "HIV_Prevalence_long_filtered_2017-07-18.csv")
+
+# Compare the numbers for each cut of data to see how many facilities are filitered out
+table(df_subset$SNU1, df_subset$timeVar)
+table(df_PEPFAR$SNU1, df_PEPFAR$timeVar)
+
